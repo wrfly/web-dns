37dfc70de4da10d53541a64d6632275eed011477
wrfly <mr.wrfly@gmail.com>
2018-02-18 20:57:59 +0800

    api

--- README.md
+++ README.md
@@ -2,12 +2,37 @@
 
 curl https://dns.kfd.me/www.google.com
 
+## API List
+
+### Default
+
+```text
+/www.google.com
+```
+
+### With Type
+
+```text
+/www.google.com/AAAA
+/www.google.com/A
+/www.google.com/MX
+```
+
+### Reurn Json
+
+```text
+/json/www.google.com
+/json/www.google.com/MX
+/json/www.google.com/AAAA
+```
+
 ## TODO
 
-- [ ] dns lib
+- [x] dns lib
 - [ ] cacher
 - [ ] docker-compose
 - [ ] metrics and debug
 - [ ] performance test
 - [ ] rate limit
 - [ ] hijack
+- [x] api list
--- route/route.go
+++ route/route.go
@@ -1,1 +1,58 @@
 package route
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"github.com/wrfly/web-dns/dig"
+)
+
+type Router struct {
+	port      int
+	blacklist []string
+	d         dig.Digger
+	e         *gin.Engine
+}
+
+func blacklistHandler(blacklist []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cip := c.GetHeader("X-Forwarded-For")
+		if cip == "" {
+			cip = c.ClientIP()
+		}
+		for _, blk := range blacklist {
+			if cip == blk {
+				c.String(http.StatusForbidden, "emm")
+				c.Abort()
+			}
+		}
+	}
+}
+
+func New(digger dig.Digger, port int, blacklist []string) *Router {
+	engine := gin.New()
+	engine.Use(blacklistHandler(blacklist))
+
+	return &Router{
+		port:      port,
+		blacklist: blacklist,
+		d:         digger,
+		e:         engine,
+	}
+}
+
+func (r *Router) Serve() {
+	logrus.Debug("server starting")
+	r.e.Any("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.e.GET("/:host")
+
+	r.e.Run(fmt.Sprintf(":%d", r.port)) // listen and serve
+}
